6b0a9d2ee108aece6c6ba2403c8f58367ad7fbc3
MrGold <dgold@sjnma.org>
2017-01-10 13:58:26 -0600

    route for list terms working,finished wrap with post function

--- participation/urls.py
+++ participation/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
+    url(r'^listTerms', views.listTerms),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,49 @@
 from django.shortcuts import render
 from config import Config
-# Create your views here.
 from django.http import HttpResponse
 import gspread
+from datetime import date
+
 
 
 from oauth2client.service_account import ServiceAccountCredentials
 
 
+def _wrapWithPost(value,route):
+    html = '''
+    <form action="{1}" method="post">
+     <input type="submit" value="{0}">
+    </form>
+    '''.format(value,route)
+    return html
+
 def index(request):
 	#HttpResponse("<h1>Hello</h1>")
 	#listStudents("test","1")
 	#listPeriods("test")
-	
+	return listTerms()
 def authenticate():
     scope = Config["scope"]
     credentials = Config["credentials"]
     gc = gspread.authorize(credentials)
     return gc
-def listTerms():
+def listTerms(request):
+    gc = authenticate()
+    currentyear = date.today().year
 
+    html = "<div id='terms'>"
+    html += str(request.POST)
+    route="/index"
+    html += _wrapWithPost(str(currentyear-1)+"Fall",route)
+    html += _wrapWithPost(str(currentyear-1)+"Spring",route)
+    html += _wrapWithPost(str(currentyear)+"Fall",route)
+    html += _wrapWithPost(str(currentyear)+"Spring",route)
+    html += _wrapWithPost(str(currentyear+1)+"Fall",route)
+    html += _wrapWithPost(str(currentyear+1)+"Spring",route)
+
+
+    html += "</div>"
+    return HttpResponse(html)
 def listPeriods(term):
     gc = authenticate()
     sheets = gc.open(term)
@@ -31,23 +55,12 @@ def listPeriods(term):
     return HttpResponse(html)
 def listStudents(term,period):
     gc = authenticate()
-
     wks = gc.open(term).worksheet(period)
-    
     students = wks.row_values(1)
     html = "<div id='students'>"
-
     students = students[2:]#clip header
     for student in students:
     	html += "<h2>"+student+"</h2>"
     html += "</div>"
-
     return HttpResponse(html)
-     
-    #gc = gspread.authorize(Config["credentials"])
-	# You can open a spreadsheet by its title as it appears in Google Doc
-    #sh = gc.open('test') # <-- Look ma, no keys!
-    #sh.worksheets()
-    #sht1 = gc.open_by_key('1incg6CDgZOyzwynSSvtG3v8oswgteG6wW3vmD6FwO6w')
-    #1incg6CDgZOyzwynSSvtG3v8oswgteG6wW3vmD6FwO6w/
     
